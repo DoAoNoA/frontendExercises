d3ac119f11c53a3ac810deebcb081bd1d58a9318
D4n1Ela <dana@correoFalso.com>
2018-04-26 01:10:32 -0300

    Front: Maqueto la página para ver que data voy a necesitar del servidor.

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  cities = [
+    {city:"nome", region:"ak"},
+    {city:"dallas", region:"tx"},
+    {city:"chicago", region:"il"}
+  ];
+
+  userCities = [
+    {userId: "1", city:"nome", region: " TX", code: "23", date: "Tue, 24 Apr 2018 08:00 PM AKDT", temp: "34", day: "Tue", high: "35", low: "31", text: "Cold"},
+    {userId: "1", city:"dallas", region: " TX", code: "23", date: "Tue, 24 Apr 2018 08:00 PM AKDT", temp: "34", day: "Tue", high: "35", low: "31", text: "Cold"},
+    {userId: "1", city:"dallas", region: " TX", code: "23", date: "Tue, 24 Apr 2018 08:00 PM AKDT", temp: "34", day: "Tue", high: "35", low: "31", text: "Cold"}
+  ];
+
 }
